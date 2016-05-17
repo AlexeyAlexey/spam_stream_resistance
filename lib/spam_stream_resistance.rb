ab42e7fe53568d0ac11338d4387e7a38a0a62998
Alexey Kondratenko <a.kondratenko@smileexpo.ru>
2016-05-17 16:28:09 +0300

    add test

--- lib/spam_stream_resistance.rb
+++ lib/spam_stream_resistance.rb
@@ -1,9 +1,54 @@
 require "spam_stream_resistance/version"
 
 class SpamStreamResistance
-  # Your code goes here...
-  def test_method
-  	byebug
-    true
+  
+  def initialize(redis_sett = { redis: {host: 'localhost', port: 6379}, connection_pool: {size: 5, timeout: 2} })
+    @redis_pool = ConnectionPool.new(redis_sett[:connection_pool]) { Redis.new(redis_sett[:redis]) }
+    
+    @settings_filter_id = {}
   end
+
+  def redis_pool
+    @redis_pool
+  end
+
+  def settings_filter_id
+    @settings_filter_id
+  end
+  #init settings of filter 
+  def init_settings_filter_id(filter_id, sett = {})
+    #{filter_id: {}}
+    @settings_filter_id[filter_id] = sett
+  end
+   
+  def settings_filter_id_add(filter_id, sett = {})
+  	#{filter_id: {settings_id: {}}, filter_id: {settings_id: {}}}
+  	@settings_filter_id[filter_id].merge!(sett)
+  end
+
+  def settings_filter_id_delete(filter_id = nil, sett_id = nil)
+  	#{filter_id: {settings_id: {}}, filter_id: {settings_id: {}}}
+    @settings_filter_id[filter_id].delete(sett_id)
+  end
+
+  def settings_filter_by_id(filter_id = nil, sett_id = nil)
+  	#{filter_id: {settings_id: {}}, filter_id: {settings_id: {}}}
+    @settings_filter_id[filter_id][sett_id]
+  end
+
+
+
+  def look_at(str, filter_id, sett_id)
+  	send("filter_#{filter_id}", filter_id, sett_id)
+  end
+  
+  def is_it_spam(str, filter_id, sett_id)
+    send("filter_#{filter_id}", filter_id, sett_id)
+  end
+
+  private
+    def filter_1(sett_id)
+ 
+      #true/false
+    end
 end
--- test/test_spam_stream_resistance.rb
+++ test/test_spam_stream_resistance.rb
@@ -1,12 +1,41 @@
 require 'minitest_helper'
 
 class TestSpamStreamResistance < Minitest::Test
+  def setup
+  	#default settings { redis: {host: 'localhost', port: 6379}, connection_pool: {size: 5, timeout: 2} }
+    @spam_stream_resistance = SpamStreamResistance.new({ redis: {host: 'localhost', port: 6379, db: 10}, connection_pool: {size: 5, timeout: 2} })
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::SpamStreamResistance::VERSION
   end
 
-  def test_it_does_something_useful
-  	SpamStreamResistance.new.test_method
-    assert false
+  def test_redis_pool
+    assert_equal "ConnectionPool", @spam_stream_resistance.redis_pool.class.name
+
+    #@spam_stream_resistance.redis_pool.with do |redis|
+    #  redis.lpush 'process_action_controller', "1"
+    #end
+    
+    #@spam_stream_resistance.redis_pool.with do |redis|
+    #  redis.lpop 'process_action_controller', "1"
+    #end
+  end
+
+  def test_settings_filter_id
+  	filter_id   = "1"
+  	settings_id = "1"
+
+    @spam_stream_resistance.init_settings_filter_id(filter_id)
+    
+    assert_equal ({ filter_id => {} }), @spam_stream_resistance.settings_filter_id
+
+    @spam_stream_resistance.settings_filter_id_add(filter_id, { settings_id => {expire: 2, max_count: 0} })
+    
+    assert_equal ( {expire: 2, max_count: 0} ), @spam_stream_resistance.settings_filter_by_id(filter_id, settings_id), "settings_filter_by_id"
+  
+    @spam_stream_resistance.settings_filter_id_delete(filter_id, settings_id)
+
+    assert_equal ({ filter_id => {} }), @spam_stream_resistance.settings_filter_id
   end
 end
